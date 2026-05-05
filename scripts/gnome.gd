9b9b5b2590bf039ec839c7dd6ba49c46e96933a6
Hubnester <ilya@neucom.ru>
2026-05-05 21:08:33 +0300

    gnomik pisaet gnomikami

--- scripts/gnome.gd
+++ scripts/gnome.gd
@@ -17,24 +17,29 @@ func _process(delta: float) -> void:
 	pass
 	
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	state.add_constant_central_force(Vector2(0, GRAVITY))
 	if !ball_mode:
 		self.rotation = 0
 	if (Input.is_action_just_released("mode")):
 		self.angular_velocity = 0
 		self.linear_velocity = Vector2.ZERO
 		ball_mode = !ball_mode
+		if !ball_mode:
+			get_node("GPUParticles2D").emitting = false
 		
 	if (Input.is_action_pressed("right")):
 		if !ball_mode:
 			self.linear_velocity.x = SPEED
 		else:
-		
+			get_node("GPUParticles2D").emitting = true
 			self.linear_velocity.x = BALL_SPEED
+			
+	if (Input.is_action_just_released("right") || Input.is_action_just_released("left")):
+		get_node("GPUParticles2D").emitting = false		
 		
 	if (Input.is_action_pressed("left")):
 		if !ball_mode:
 			self.linear_velocity.x = -SPEED
 		else:
+			get_node("GPUParticles2D").emitting = true
 			self.linear_velocity.x = -BALL_SPEED
 		
